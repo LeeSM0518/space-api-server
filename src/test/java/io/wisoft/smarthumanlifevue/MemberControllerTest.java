e911f7e738c90cc10a686f5bace7b40f8b1f3096
Sangmin Lee <nalsm98@naver.com>
2020-11-02 23:54:51 +0900

    upload

--- src/test/java/io/wisoft/smarthumanlifevue/MemberControllerTest.java
+++ src/test/java/io/wisoft/smarthumanlifevue/MemberControllerTest.java
@@ -1,68 +1,79 @@
-//package io.wisoft.smarthumanlifevue;
-//
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.jupiter.params.ParameterizedTest;
-//import org.junit.jupiter.params.provider.CsvSource;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.MvcResult;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@WebMvcTest
-//public class MemberControllerTest {
-//
-//  @Autowired
-//  private MockMvc mockMvc;
-//  @Autowired
-//  private ObjectMapper objectMapper;
-//
-//  @ParameterizedTest
-//  @CsvSource({
-//      "nalsm12, 1234, 1998.05.18, 정보통신공학과, 010-7188-9608, nalsm0518@gmail.com, 안녕하세요!",
-//  })
-//  public void login(String username, String password, String birthday, String department,
-//                     String phone, String email, String introduction) throws Exception {
-//    // given
-//    SignupDto build = SignupDto.builder()
-//        .username(username)
-//        .password(password)
-//        .birthday(birthday)
-//        .department(department)
-//        .phone(phone)
-//        .email(email)
-//        .introduction(introduction)
-//        .build();
-////    memberService.signUp(build);
-//
-////    RequestToLoginDto requestToLoginDto = RequestToLoginDto.builder()
-////        .username(username)
-////        .password(password)
-////        .build();
-//
-//    // when
-//    MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("https://smarthumanlife.herokuapp.com/signup")
-//    .contentType(MediaType.APPLICATION_JSON)
-//    .accept(MediaType.APPLICATION_JSON)
-//    .characterEncoding("UTF-8")
-//    .content(objectMapper.writeValueAsString(build)))
-//        .andReturn();
-//
-//    // then
-//    int status = result.getResponse().getStatus();
-//    assertEquals(HttpStatus.OK.value(), status);
-////    ResponseToLoginDto responseToLoginDto =
-////        objectMapper.readValue(result.getResponse().getContentAsString(), ResponseToLoginDto.class);
-////    assertEquals(birthday, responseToLoginDto.getBirthday());
-//  }
-//
-//
-//
-//}
+package io.wisoft.smarthumanlifevue;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+
+  @ParameterizedTest
+  @CsvSource({
+      "nalsm12, 이상민, 1234, 1998.05.18, 정보통신공학과, 010-7188-9608, nalsm0518@gmail.com, 안녕하세요!",
+  })
+  @Rollback(value = false)
+  public void login(String username, String name, String password, String birthday, String department,
+                     String phone, String email, String introduction) throws Exception {
+    // given
+    SignupDto build = SignupDto.builder()
+        .username(username)
+        .name(name)
+        .password(password)
+        .birthday(birthday)
+        .department(department)
+        .phone(phone)
+        .email(email)
+        .introduction(introduction)
+        .build();
+
+    RequestToLoginDto requestToLoginDto = RequestToLoginDto.builder()
+        .username(username)
+        .password(password)
+        .build();
+
+    // when
+    MvcResult signupResult = mockMvc.perform(MockMvcRequestBuilders.post("/signup")
+    .contentType(MediaType.APPLICATION_JSON)
+    .accept(MediaType.APPLICATION_JSON)
+    .characterEncoding("UTF-8")
+    .content(objectMapper.writeValueAsString(build)))
+        .andReturn();
+
+    // then
+    int status = signupResult.getResponse().getStatus();
+    assertEquals(HttpStatus.OK.value(), status);
+
+    MvcResult loginResult = mockMvc.perform(MockMvcRequestBuilders.post("/login")
+        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+        .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+        .characterEncoding("UTF-8")
+        .content(objectMapper.writeValueAsString(requestToLoginDto)))
+        .andReturn();
+
+    ResponseToLoginDto responseToLoginDto =
+        objectMapper.readValue(loginResult.getResponse().getContentAsString(), ResponseToLoginDto.class);
+    assertEquals(name, responseToLoginDto.getName());
+  }
+
+
+
+}
--- src/test/java/io/wisoft/smarthumanlifevue/MemberRepositoryTest.java
+++ src/test/java/io/wisoft/smarthumanlifevue/MemberRepositoryTest.java
@@ -1,17 +1,12 @@
 package io.wisoft.smarthumanlifevue;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Date;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -28,6 +23,7 @@ public class MemberRepositoryTest {
       "nalsm98, 이상민, 1234, 1998.05.18, 정보통신공학과, 010-7188-9608, nalsm0518@gmail.com, 안녕하세요!",
   })
   @Rollback(value = false)
+  @Disabled
   public void signup(String name, String username, String password, String birthday, String department,
                      String phone, String email, String introduction) throws Exception {
     // given
